8618c9e7a28a5584903a113b404d752a68402761
havleoo <hung28092003@gmail.com>
2024-12-23 19:20:43 +0700

    Initial commit

---
+++ app/src/main/java/com/example/filemanagerapp/FileContentActivity.kt
@@ -0,0 +1,20 @@
+package com.example.filemanagerapp
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import java.io.File
+
+class FileContentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_file_content)
+
+        val filePath = intent.getStringExtra("filePath")
+        val fileContent = File(filePath!!).readText()
+
+        val textView: TextView = findViewById(R.id.textView)
+        textView.text = fileContent
+    }
+}
